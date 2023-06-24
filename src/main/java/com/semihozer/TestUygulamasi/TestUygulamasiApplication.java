24b9b9208f4ba4f31e8f33478dcfee47e651950c
Semih OZER <semihozer.smhozr@gmail.com>
2023-06-24 17:20:33 +0300

    initializing

---
+++ src/main/java/com/semihozer/TestUygulamasi/TestUygulamasiApplication.java
@@ -0,0 +1,13 @@
+package com.semihozer.TestUygulamasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestUygulamasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestUygulamasiApplication.class, args);
+	}
+
+}
